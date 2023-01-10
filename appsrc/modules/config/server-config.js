6a5df25c3b6e34b2fc99d3f8e5ca130e5eaa89dc
haris ahmad <harisahmaad2gmail.com>
2023-01-10 18:35:47 +0500

    updates

---
+++ appsrc/modules/config/server-config.js
@@ -0,0 +1,60 @@
+const apiPath = process.env.API_ROOT;
+
+const fs         = require('fs');
+const path       = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// MIDDLEWARE
+const setHeaders = require('../../middleware/set-header');
+const errorHandler = require('../../middleware/error-handler');
+
+// ROUTES
+const usersRoutes = require('../user/routes/user-route');
+const assetsRoutes = require('../asset/routes/asset-route');
+const departmentRoutes = require('../department/routes/department-route');
+const locationRoutes = require('../customer/routes/location-route');
+
+
+/**
+ * This class is the main App entry point.
+ *
+ * It sets up
+ *  - DB connection to Mongo
+ *  - Express middleware
+ */
+class App {
+  /**
+   * - Assigns Express app object to this
+   *
+   * @param  app An ExpressJS app object
+   */
+  constructor() {
+    this.app = express();
+    this.app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+    this.app.use(setHeaders);
+    this.app.use(bodyParser.json());
+    this.registerRoutes();
+    this.app.use(errorHandler);
+  }
+
+  registerRoutes(){
+    this.app.use(`${ apiPath }/users`, usersRoutes);
+    this.app.use(`${ apiPath }/assets`, assetsRoutes);
+    this.app.use(`${ apiPath }/locations`, locationRoutes);
+    this.app.use(`${ apiPath }/departments`, departmentRoutes);
+  }
+
+  start(){
+        try {
+          this.app.listen({port: process.env.PORT || 3001}, () => {
+            console.log(`Listening at  http://${process.env.HOST_NAME}:${process.env.PORT}/`)
+          })
+
+        } catch (error) {
+          console.log(error);
+        }    
+  }
+}
+
+module.exports = App;
