691d59180e249ba6dca51731a5b70fbee79801de
awais <awais@terminustech.com>
2023-05-09 13:29:55 +0500

    changes for database added promises if callback is not present

--- appsrc/modules/db/dbService.js
+++ appsrc/modules/db/dbService.js
@@ -5,78 +5,112 @@ class dbService {
   constructor() {
   }
 
-  getObjectById(model, fields, id, populate, callback) {
+  async getObjectById(model, fields, id, populate, callback) {
     //console.log('populate :'+populate);
-
-    model.findById(id, fields).populate(populate).exec((err, documents) => {
-      if (err) {
-        callback(err, {});
-      } else {
-        callback(null, documents != null ? documents : {});
-      }
-    });
+    if(callback) {
+      model.findById(id, fields).populate(populate).exec((err, documents) => {
+        if (err) {
+          callback(err, {});
+        } else {
+          callback(null, documents != null ? documents : {});
+        }
+      });
+    }
+    else {
+      return await model.findById(id, fields).populate(populate);
+    }
   }
 
-  getObject(model, query, populate, callback) {
-    model.findOne(query).populate(populate).exec((err, document) => {
-      if (err) {
-        callback(err, {});
-      } else {
-        callback(null, document || {});
-      }
-    });
+  async getObject(model, query, populate, callback) {
+    if(callback) {
+      model.findOne(query).populate(populate).exec((err, document) => {
+        if (err) {
+          callback(err, {});
+        } else {
+          callback(null, document || {});
+        }
+      });
+    }
+    else {
+      return await model.findOne(query).populate(populate);
+    }
   }
 
-  getObjectList(model, fields, query, orderBy, populate, callback) {
-    model.find(query).collation({locale: "en"}).select(fields).populate(populate).sort(orderBy).exec((err, documents) => {
-      if (err) {
-        callback(err, []);
-      } else {
-        callback(null, documents != null ? documents : []);
-      }
-    });
+  async getObjectList(model, fields, query, orderBy, populate, callback) {
+    if(callback) {
+      model.find(query).collation({locale: "en"}).select(fields).populate(populate).sort(orderBy).exec((err, documents) => {
+        if (err) {
+          callback(err, []);
+        } else {
+          callback(null, documents != null ? documents : []);
+        }
+      });
+    }
+    else {
+      return await model.find(query).collation({locale: "en"}).select(fields).populate(populate).sort(orderBy);
+    }
   }
 
 
-  getObjectListWithAggregate(model, aggregate, params, callback) {
-    model.aggregate([
-      aggregate
-    ]).exec((err, documents) => {
-      if (err) {
-        callback(err, []);
-      } else {
-        callback(null, documents != null ? documents : []);
-      }
-    });
+  async getObjectListWithAggregate(model, aggregate, params, callback) {
+    if(callback) {
+      model.aggregate([
+        aggregate
+      ]).exec((err, documents) => {
+        if (err) {
+          callback(err, []);
+        } else {
+          callback(null, documents != null ? documents : []);
+        }
+      });
+    }
+    else {
+      return await model.aggregate([aggregate]);
+    }
   }
 
-  deleteObject(model, id, callback) {
-    model.deleteOne({ _id: id }).then(function (result) {
-      callback(null, result);
-    }).catch(function (err) {
-      callback(err);
-    });
+  async deleteObject(model, id, callback) {
+    if(callback) {
+      model.deleteOne({ _id: id }).then(function (result) {
+        callback(null, result);
+      }).catch(function (err) {
+        callback(err);
+      });
+    }
+    else {
+      return await model.deleteOne({ _id: id });
+    }
   }
 
-  postObject(Object, callback) {
-    Object.save((error, data) => {
-      if (error) {
+  async postObject(Object, callback) {
+    if(callback) {
+      Object.save((error, data) => {
+        if (error) {
+          console.error(error);
+          callback(error);
+        } else {
+          callback(null, data);
+        }
+      });
+    }
+    else {
+      return await Object.save(); 
+    }
+  }
+
+  async patchObject(model, id, Object, callback) {
+    if(callback) {
+      model.updateOne({ _id: id }, Object).then(function (doc) {
+        //console.log("doc: "+JSON.stringify(doc) );
+        callback(null, doc);
+      }).catch(function (error) {
         console.error(error);
         callback(error);
-      } else {
-        callback(null, data);
-      }
-    });
-  }
-
-  patchObject(model, id, Object, callback) {
-    model.updateOne({ _id: id }, Object).then(function (doc) {
-      //console.log("doc: "+JSON.stringify(doc) );
-      callback(null, doc);
-    }).catch(function (error) {
-      console.error(error);
-      callback(error);
-    });
+      });
+    }
+    else{
+      return await model.updateOne({ _id: id }, Object);
+    }
   }
 }
 
