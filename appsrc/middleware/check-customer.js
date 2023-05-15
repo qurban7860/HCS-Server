3c3847283f4b200dcda108b1a9dcf50e4b7292f6
Zain Farrukh <zainfarrukh01@gmail.com>
2023-05-15 09:53:41 +0500

    middleware bug fixed

--- appsrc/middleware/check-customer.js
+++ appsrc/middleware/check-customer.js
@@ -11,27 +11,29 @@ module.exports = (req, res, next) => {
     const token = req && req.headers && req.headers.authorization ? req.headers.authorization.split(' ')[1]:''; // Authorization: 'Bearer TOKEN'
 
     if (!token || token.length == 0) {
-      throw new Error('Authentication failed!');
+      next();
     }
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRETKEY);
-    const { userId } = decodedToken;
-    
-    SecurityUser.findById(userId)
-    .populate({ path: 'customer', select: 'name type isActive isArchived' })
-    .exec(function (err, record) {
-      if (err) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err);
-      }
-      if (!record) {
-        return res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Customer not found!', true));
-      } else {
-          if (_.isEmpty(record.customer) || record.customer.type != 'SP' || record.customer.isActive == false || record.customer.isArchived == true) {
-            res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Customer verifcation failed!', true));
-          }else{
-            next();
-          }        
-      }
-    });
+    else{
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRETKEY);
+      const { userId } = decodedToken;
+      
+      SecurityUser.findById(userId)
+      .populate({ path: 'customer', select: 'name type isActive isArchived' })
+      .exec(function (err, record) {
+        if (err) {
+          return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err);
+        }
+        if (!record) {
+          return res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Customer not found!', true));
+        } else {
+            if (_.isEmpty(record.customer) || record.customer.type != 'SP' || record.customer.isActive == false || record.customer.isArchived == true) {
+              res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Customer verifcation failed!', true));
+            }else{
+              next();
+            }        
+        }
+      });
+    }    
   } catch (err) {
     console.log(err);
     const error = new HttpError('Authentication failed!', 403);
