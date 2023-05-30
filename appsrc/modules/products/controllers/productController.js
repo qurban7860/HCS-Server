a554977c66f5e58e19ccf4ece903bd073c5fd72f
awais <awais@terminustech.com>
2023-05-30 11:13:48 +0500

    clone added

--- appsrc/modules/products/controllers/productController.js
+++ appsrc/modules/products/controllers/productController.js
@@ -49,6 +49,8 @@ exports.getProduct = async (req, res, next) => {
       if(machine && Array.isArray(machine.machineConnections) && machine.machineConnections.length>0) {
         let query_ = { _id : { $in:machine.machineConnections }, isActive : true, isArchived : false };
         let populate = {path: 'connectedMachine', select: '_id name serialNo'}
+        machine = JSON.parse(JSON.stringify(machine));
+
 
         let machineConnections = await dbservice.getObjectList(ProductConnection,this.fields, query_, {}, populate);
         console.log(machineConnections);
