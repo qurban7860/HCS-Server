be28cbe3ebda4a472c3b84bfc767f64d3876e701
awais <awais@terminustech.com>
2023-07-13 10:05:55 +0500

    changes for dashboard

--- appsrc/modules/dashboard/controllers/dashboardController.js
+++ appsrc/modules/dashboard/controllers/dashboardController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const { ReasonPhrases, StatusCodes, getReasonPhrase, getStatusCode } = require('http-status-codes');
 const { Customer, CustomerSite } = require('../../crm/models');
-const { Product } = require('../../products/models');
+const { Product, ProductModel } = require('../../products/models');
 const { SecurityUser } = require('../../security/models');
 const HttpError = require('../../config/models/http-error');
 const logger = require('../../config/logger');
@@ -15,7 +15,9 @@ exports.getData = async (req, res, next) => {
 
   try{
     let customerCount = await Customer.find({isActive:true, isArchived:false}).countDocuments();
+    let nonVerifiedCustomerCount = await Customer.find({isActive:true, isArchived:false,"verifications.0":{$exists:false}}).countDocuments();
     let machineCount = await Product.find({isActive:true, isArchived:false}).countDocuments();
+    let nonVerifiedMachineCount = await Product.find({isActive:true, isArchived:false,"verifications.0":{$exists:false}}).countDocuments();
     let userCount = await SecurityUser.find({isActive:true, isArchived:false}).countDocuments();
     let siteCount = await CustomerSite.find({isActive:true, isArchived:false}).countDocuments();
     
@@ -29,18 +31,25 @@ exports.getData = async (req, res, next) => {
       { $limit: 20 }
     ]);
     
-    let countryWiseCustomerCount = await Customer.aggregate([
-      { $match: { isArchived: false, isActive: true } }, 
-      { $lookup: { from: "CustomerSites", localField: "mainSite", foreignField: "_id", as: "mainSite" } },
-      { $unwind: "$mainSite" },
-      { $match: { "mainSite.address.country": { $nin: ["", null] } } },
-      { $group: { _id: "$mainSite.address.country", count: { $sum: 1 } } },
-      { $sort: { count: -1 } },
-      { $limit: 20 }
+    // let countryWiseCustomerCount = await Customer.aggregate([
+    //   { $match: { isArchived: false, isActive: true } }, 
+    //   { $lookup: { from: "CustomerSites", localField: "mainSite", foreignField: "_id", as: "mainSite" } },
+    //   { $unwind: "$mainSite" },
+    //   { $match: { "mainSite.address.country": { $nin: ["", null] } } },
+    //   { $group: { _id: "$mainSite.address.country", count: { $sum: 1 } } },
+    //   { $sort: { count: -1 } },
+    //   { $limit: 20 }
+    // ]);
+
+    let machineModels = await ProductModel.aggregate([
+      { $lookup: { from: "MachineCategories", localField: "category", foreignField: "_id", as: "machineCategory" } },
+      { $match: { "machineCategory.connections": true} },
     ]);
+    let modelsIds = machineModels.map(m => m._id);
+    let connectAbleMachinesCount = await Product.find({machineModel:{$in:modelsIds}}).countDocuments();
 
     let countryWiseMachineCount = await Product.aggregate([
-      { $match: { isArchived: false, isActive: true } }, 
+      { $match: { isArchived: false, isActive: true,  machineModel:{$nin:modelsIds} } }, 
       { $lookup: { from: "CustomerSites", localField: "instalationSite", foreignField: "_id", as: "instalationSite" } },
       { $unwind: "$instalationSite" },
       { $match: { "instalationSite.address.country": { $nin: ["", null] } } },
@@ -49,17 +58,29 @@ exports.getData = async (req, res, next) => {
       { $limit: 20 }
     ]);
 
-    let countryWiseSiteCount = await CustomerSite.aggregate([
-      { $match: { isArchived: false, isActive: true, 
-          "address.country": { $nin: ["", null] } 
-        }
-      },
-      { $group: { _id: "$address.country", count: { $sum: 1 }}}
-    ]);
+    // let countryWiseSiteCount = await CustomerSite.aggregate([
+    //   { $match: { isArchived: false, isActive: true, 
+    //       "address.country": { $nin: ["", null] } 
+    //     }
+    //   },
+    //   { $group: { _id: "$address.country", count: { $sum: 1 }}}
+    // ]);
 
-  res.json({customerCount, machineCount, userCount, siteCount, modelWiseMachineCount, countryWiseCustomerCount, countryWiseSiteCount, countryWiseMachineCount});
+  res.json({customerCount, 
+    nonVerifiedCustomerCount, 
+    nonVerifiedMachineCount,
+    connectAbleMachinesCount,
+    machineCount, 
+    userCount, 
+    siteCount, 
+    modelWiseMachineCount, 
+    // countryWiseCustomerCount, 
+    // countryWiseSiteCount, 
+    countryWiseMachineCount
+  });
 
   }catch(e) {
+    console.log(e);
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR));
   }
 
