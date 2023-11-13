3aa0e8f4f871928b577318d6614c3902b91e521e
haris@terminustech.com <Haris Ahmad>
2023-11-13 15:23:41 +0500

    testing console added.

--- appsrc/modules/products/controllers/productServiceRecordsController.js
+++ appsrc/modules/products/controllers/productServiceRecordsController.js
@@ -113,7 +113,7 @@ exports.getProductServiceRecord = async (req, res, next) => {
               });
 
               if(PSRV) {
-                console.log("PSRV.checkItemValue", PSRV.checkItemValue);
+                console.log("PSRV.checkItemValue", PSRV.checkItemValue, productCheckItemObject.name);
                 productCheckItemObject.serviceRecord = PSRV.serviceRecord;
                 productCheckItemObject.checkItemValue = PSRV.checkItemValue+'';
                 productCheckItemObject.comments = PSRV.comments;
