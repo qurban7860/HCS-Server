d4f6bf6f849d3429f79500f9cdb60e69d916b552
haris@terminustech.com <Haris Ahmad>
2023-11-20 16:23:49 +0500

    product configurations.

---
+++ appsrc/modules/products/models/productConfiguration.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const { softDeletePlugin } = require('soft-delete-plugin-mongoose');
+const baseSchema = require('../../../base/baseSchema');
+
+const GUID = require('mongoose-guid')(mongoose);
+
+
+const Schema = mongoose.Schema;
+
+const docSchema = new Schema({
+    inputGUID: { type: String , required: true },
+
+    inputSerialNo: { type: String , required: true },
+    // Serial No of machine
+
+    machine: { type: Schema.Types.ObjectId, ref: 'Machine' },
+
+    configuration: { type: Schema.Types.Mixed },
+    // configuration Object Received.
+},
+{
+    collection: 'MachineConfigurations'
+});
+docSchema.set('timestamps', true);
+docSchema.add(baseSchema.docVisibilitySchema);
+docSchema.add(baseSchema.docAuditSchema);
+
+docSchema.plugin(uniqueValidator);
+
+docSchema.index({"machine":1})
+docSchema.index({"serialNo":1})
+docSchema.index({"isActive":1})
+docSchema.index({"isArchived":1})
+
+module.exports = mongoose.model('ProductConfiguration', docSchema);
