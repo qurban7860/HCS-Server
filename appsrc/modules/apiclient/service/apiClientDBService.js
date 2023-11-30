55b90c3b0d757db10ca78f9e47d69bddbd01942b
haris@terminustech.com <Haris Ahmad>
2023-11-30 15:33:35 +0500

    updated with call back.

--- appsrc/modules/apiclient/service/apiClientDBService.js
+++ appsrc/modules/apiclient/service/apiClientDBService.js
@@ -11,76 +11,85 @@ class APIClientService {
     this.db = new dbService();
   }
 
-  // getObject(model, query, populate, callback) {
-  //   this.db.getObject(model, query, populate, callbackFunc);
-  //   function callbackFunc(error, response) {
-  //     if (error) callback(error, {});
-  //     else callback(null, response);
-  //   }
-  // }{}
-  
-  async getObject(model, query, populate) {
-    try {
-      const response = await this.db.getObject(model, query, populate);
-      return response;
-    } catch (error) {
-      return error;
-    }
-  }
-
-  async getObjectById(model, fields, id, populate) {
-    try {
-      const response = await this.db.getObjectById(model, fields, id, populate);
-      return response;
-    } catch (error) {
-      return error;
-    }
-  }
-
-  async getObjectList(model, fields, query, orderBy, populate) {
-    try {
-      const response = await this.db.getObjectList(model, fields, query, orderBy, populate);
-      return response;
-    } catch (error) {
-      return error;
-    }
-  }
-
-  async deleteObject(model, id, res, callback){
-    try{
-      if(callback){
-        this.db.deleteObject(model, id, res, callbackFunc);
-        function callbackFunc(error, response) {
-          if (error) callback(error, {});
-          else callback(null, response);
-        }
-      }else{
-        return await this.db.deleteObject(model, id, res);
+  async getObject(model, query, populate, callback) {
+    if(callback) {
+      this.db.getObject(model, query, populate, callbackFunc);
+      function callbackFunc(error, response) {
+        if (error) callback(error, {});
+        else callback(null, response);
       }
-    } catch(error) {
-      return error;
+    }
+    else {
+      return await this.db.getObject(model, query, populate);
     }
   }
 
-  async postObject(document){
-    try{
-      const response = await this.db.postObject(document);
-      return response;
+  async getObjectById(model, fields, id, populate, callback) {
+    if(callback) {
+      this.db.getObjectById(model, fields, id, populate, callbackFunc);
+      function callbackFunc(error, response) {
+        if (error) callback(error, {});
+        else callback(null, response);
+      }
     }
-    catch(error){
-      return error;
-    }
-  }
-  
-  async patchObject(model, id, document){
-    try{
-      const response = await this.db.patchObject(model, id, document);
-      return response;
-    }
-    catch(error){
-      return error;
+    else {
+      return await this.db.getObjectById(model, fields, id, populate);
     }
   }
+
+  async getObjectList(model, fields, query, orderBy, populate, callback) {
+    if(callback) {
+      this.db.getObjectList(model, fields, query, orderBy, populate, callbackFunc);
+      function callbackFunc(error, response) {
+        if (error) callback(error, {});
+        else callback(null, response);
+      }
+    }
+    else {
+      return await this.db.getObjectList(model, fields, query, orderBy, populate);
+    }
+  };
+
+  async deleteObject(model, id, res, callback) {
+    if(callback) {
+      this.db.deleteObject(model, id, res, callbackFunc);
+      function callbackFunc(error, response) {
+        if (error) callback(error, {});
+        else callback(null, response);
+      }
+    }
+    else {
+      return await this.db.deleteObject(model, id, res);
+    }
+  };
+
+  async postObject(document, callback) {
+    if(callback) {
+      this.db.postObject(document, callbackFunction);
+      function callbackFunction(error, response) {
+        if (error) callback(error, {});
+        else callback(null, response);
+      }
+    }
+    else {
+      return await this.db.postObject(document);
+    }
+  }
+
+  async patchObject(model, id, document, callback) {
+    if(callback) {
+      this.db.patchObject(model, id, document, callbackFunc);
+      function callbackFunc(error, result) {
+        if (error) callback(error, {});
+        else callback(null, result);
+      }
+    }
+    else {
+      return await this.db.patchObject(model, id, document);
+    }
+  };
+
+
 }
 
 
