823c2d2f268afe6de49c4387fcb1b7820071f110
haris@terminustech.com <Haris Ahmad>
2023-12-06 12:26:32 +0500

    update.

--- appsrc/modules/products/controllers/productServiceRecordsController.js
+++ appsrc/modules/products/controllers/productServiceRecordsController.js
@@ -438,6 +438,7 @@ async function addEmail(subject, body, toUser, emailAddresses, fromEmail='', ccE
   if(toUser && mongoose.Types.ObjectId.isValid(toUser.id)) {
     email.toUsers.push(toUser.id);
 
+    console.log("customer", toUser.customer);
     if(toUser.customer && toUser.customer.id && mongoose.Types.ObjectId.isValid(toUser.customer.id)) {
       email.customer = toUser.customer.id;
     }
