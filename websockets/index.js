465eafbcca6bc4751c8e1925e293a0bf843943ab
haris@terminustech.com <Haris Ahmad>
2023-12-19 17:34:42 +0500

    sorting

--- websockets/index.js
+++ websockets/index.js
@@ -81,7 +81,8 @@ WebSocket.on('connection', async function(ws, req) {
         let sendEventData = {};
         if(eventName=='getNotifications') {
             let queryString__ =  {receivers:userId, isActive: true, isArchived: false};
-            let notifications = await SecurityNotification.find(queryString__).sort({_id: -1}).populate('sender');
+            let notifications = await SecurityNotification.find(queryString__).sort({createdAt: -1}).populate('sender');
+            console.log("notifications", notifications);
             sendEventData = { eventName:'notificationsSent', data : notifications };
             emitEvent(ws, sendEventData);
         }
