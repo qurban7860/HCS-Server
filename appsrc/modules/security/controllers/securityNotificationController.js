291625ca4e60527c2247c710d4fe84387a249f2e
haris@terminustech.com <Haris Ahmad>
2023-12-19 12:27:01 +0500

    notification issue solved.

--- appsrc/modules/security/controllers/securityNotificationController.js
+++ appsrc/modules/security/controllers/securityNotificationController.js
@@ -123,9 +123,9 @@ exports.createNotification = async(description='', sender='', receiver='', type=
 
     console.log("@1 -------------->", notification);
     
-    if(Array.isArray(notification.receiver) && notification.receiver.length>0) {
-      console.log("@2", notification?.receiver?.length);
-      notification.receiver.forEach((notifRecevier)=>{
+    if(Array.isArray(notification.receivers) && notification.receivers.length>0) {
+      console.log("@2", notification?.receivers?.length);
+      notification.receivers.forEach((notifRecevier)=>{
         console.log("notifRecevier", notifRecevier);
         const wss = getSocketConnectionByUserId(notifRecevier);
 
