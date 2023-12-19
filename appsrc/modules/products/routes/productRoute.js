aadd999aaca1f2f4171bc4153308f5c3fd40250c
haris@terminustech.com <Haris Ahmad>
2023-12-19 14:57:52 +0500

    updated.

--- appsrc/modules/products/routes/productRoute.js
+++ appsrc/modules/products/routes/productRoute.js
@@ -21,29 +21,29 @@ const baseRouteForObject = `/machines`;
 // localhost://api/1.0.0/products/machines/ 
 //localhost://api/1.0.0/products/search/
 
-router.use(checkAuth, checkCustomer, checkcustomerexcludeReports);
+router.use(checkAuth, checkCustomer);
 
-router.get(`${baseRouteForObject}/export`, controller.exportProductsJSONforCSV);
+router.get(`${baseRouteForObject}/export`, checkcustomerexcludeReports, controller.exportProductsJSONforCSV);
 
-router.get(`${baseRouteForObject}/`, controller.getProducts);
+router.get(`${baseRouteForObject}/`, checkcustomerexcludeReports, controller.getProducts);
 
-router.get(`${baseRouteForObject}/getMachinesAgainstCountries`, controller.getMachinesAgainstCountries);
+router.get(`${baseRouteForObject}/getMachinesAgainstCountries`, checkcustomerexcludeReports, controller.getMachinesAgainstCountries);
 
-router.get(`${baseRouteForObject}/machineCoordinates`, controller.getProductsSiteCoordinates);
+router.get(`${baseRouteForObject}/machineCoordinates`, checkcustomerexcludeReports, controller.getProductsSiteCoordinates);
 
-router.get(`${baseRouteForObject}/getDecoilerProducts/`, controller.getConnectionProducts); 
+router.get(`${baseRouteForObject}/getDecoilerProducts/`, checkcustomerexcludeReports, controller.getConnectionProducts); 
 
-router.get(`${baseRouteForObject}/:id`, controller.getProduct);
+router.get(`${baseRouteForObject}/:id`, checkcustomerexcludeReports, controller.getProduct);
 
-router.post(`${baseRouteForObject}`, controller.postProduct);
+router.post(`${baseRouteForObject}`, checkcustomerexcludeReports, controller.postProduct);
 
-router.patch(`${baseRouteForObject}/:id`, verifyDelete, controller.patchProduct);
+router.patch(`${baseRouteForObject}/:id`, verifyDelete, checkcustomerexcludeReports, controller.patchProduct);
 
-router.delete(`${baseRouteForObject}/:id`, controller.deleteProduct);
+router.delete(`${baseRouteForObject}/:id`, checkcustomerexcludeReports, controller.deleteProduct);
 
-router.post(`${baseRouteForObject}/transferMachine/`, controller.transferOwnership);
+router.post(`${baseRouteForObject}/transferMachine/`, checkcustomerexcludeReports, controller.transferOwnership);
 
-router.post(`${baseRouteForObject}/moveMachine/`, controller.moveMachine);
+router.post(`${baseRouteForObject}/moveMachine/`, checkcustomerexcludeReports, controller.moveMachine);
 
 
 module.exports = router;
