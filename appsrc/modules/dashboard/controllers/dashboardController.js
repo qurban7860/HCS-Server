49705c2146314fb09da4734e199beefe2abc2bcd
haris@terminustech.com <Haris Ahmad>
2023-12-20 15:47:10 +0500

    exclude report machines.

--- appsrc/modules/dashboard/controllers/dashboardController.js
+++ appsrc/modules/dashboard/controllers/dashboardController.js
@@ -305,8 +305,14 @@ exports.getData = async (req, res, next) => {
     let customerCount = await Customer.find({isActive:true, isArchived:false}).countDocuments();
     let nonVerifiedCustomerCount = await Customer.find({isActive:true, isArchived:false,"verifications.0":{$exists:false}}).countDocuments();
     let excludeReportingCustomersCount = await Customer.find({isActive:true, isArchived:false, excludeReports: true}).countDocuments();
-    let machineCount = await Product.find({isActive:true, isArchived:false}).countDocuments();
-    let nonVerifiedMachineCount = await Product.find({isActive:true, isArchived:false,"verifications.0":{$exists:false}}).countDocuments();
+    
+    let listCustomers = await Customer.find({"excludeReports": { $ne: false }}).select('_id').lean();
+    let machineCountQuery = {isActive:true, isArchived:false, customer: { $in: listCustomers }};
+    let machineCount = await Product.find(machineCountQuery).countDocuments();
+
+    let nonVerifiedMachineCountQuery = {isActive:true, isArchived:false,"verifications.0":{$exists:false}, customer: { $in: listCustomers } };
+    let nonVerifiedMachineCount = await Product.find(nonVerifiedMachineCountQuery).countDocuments();
+    
     let userTotalCount = await SecurityUser.find({isArchived:false}).countDocuments();
     let userActiveCount = await SecurityUser.find({isActive:true, isArchived:false}).countDocuments();
     let siteCount = await CustomerSite.find({isActive:true, isArchived:false}).countDocuments();
