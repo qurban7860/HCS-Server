9f33dfc65350a2fd18f9677d8bc93b1cc6c4772f
Haris Ahmad <haris@terminustech.com>
2024-01-16 15:29:40 +0500

    test

--- appsrc/modules/documents/routes/documentVersionRoute.js
+++ appsrc/modules/documents/routes/documentVersionRoute.js
@@ -45,16 +45,6 @@ router.post(`${baseRoute}/:documentid/versions/`, (req, res, next) => {
         console.log(err);
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR));
       } else {
-        const regex = new RegExp("^OPTIMIZE_IMAGE$", "i"); let configObject = await Config.findOne({name: regex, type: "ADMIN-CONFIG", isArchived: false, isActive: true}).select('value'); configObject = configObject && configObject.value.trim().toLowerCase() === 'true' ? true:false;
-        if(req.files && req.files['images']) {
-          const documents_ = req.files['images'];
-          await Promise.all(documents_.map(async (docx, index) => {
-            if(configObject){
-              await awsService.processImageFile(docx);
-              docx.eTag = await awsService.generateEtag(docx.path);
-            }
-          }));
-        }
         next();
       }
     });
