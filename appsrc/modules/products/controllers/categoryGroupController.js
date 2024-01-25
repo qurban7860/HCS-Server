2f4728d8b7468cd59b98ea5165d8e3191cd88d35
Haris Ahmad <haris@terminustech.com>
2024-01-25 15:45:18 +0500

    updated functions.

--- appsrc/modules/products/controllers/categoryGroupController.js
+++ appsrc/modules/products/controllers/categoryGroupController.js
@@ -55,7 +55,6 @@ exports.getCategoryGroups = async (req, res, next) => {
       logger.error(new Error(error));
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR));
     } else {
-      console.log("response");
       res.json(response);
     }
   }
