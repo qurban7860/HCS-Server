85948a8634275f73756af6cb85a864a034c8c744
Haris Ahmad <haris@terminustech.com>
2024-02-01 17:46:50 +0500

    test

--- appsrc/base/aws.js
+++ appsrc/base/aws.js
@@ -154,7 +154,7 @@ async function sendEmail(params) {
         Data: params.subject
        }
       },
-    Source: "Howick Limited",
+    Source: process.env.AWS_SES_FROM_EMAIL, /* required */
     ReplyToAddresses: [
       process.env.AWS_SES_FROM_EMAIL,
     ],
