6f8f11368897ed00eff763cf12b1c829c8e7324d
Haris Ahmad <haris@terminustech.com>
2024-02-02 17:45:36 +0500

    removed console.

--- appsrc/modules/products/controllers/productController.js
+++ appsrc/modules/products/controllers/productController.js
@@ -346,7 +346,7 @@ exports.getProducts = async (req, res, next) => {
   }
 
 
-  console.log(JSON.stringify(this.query));
+  // console.log(JSON.stringify(this.query));
   dbservice.getObjectList(req, Product, listFields, this.query, this.orderBy, listPopulate, callbackFunc);
   async function callbackFunc(error, products) {
     if (error) {
