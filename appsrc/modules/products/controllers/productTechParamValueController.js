baa8d6909d5976844a15cb448ee6b629061b3a43
Haris Ahmad <haris@terminustech.com>
2024-03-14 17:55:50 +0500

    multiple machines.

--- appsrc/modules/products/controllers/productTechParamValueController.js
+++ appsrc/modules/products/controllers/productTechParamValueController.js
@@ -91,7 +91,24 @@ exports.postProductTechParamValue = async (req, res, next) => {
   if (!errors.isEmpty()) {
     res.status(StatusCodes.BAD_REQUEST).send(getReasonPhrase(StatusCodes.BAD_REQUEST));
   } else {
+
+
     const objectVal = await exports.getDocumentFromReq(req, 'new');
+
+    if (Array.isArray(req.body.machines)) {
+      for (let i = 0; i < req.body.machines.length; i++) {
+        const machine = req.body.machines[i];
+        req.body.machine = machine;
+        const objectVal_ = await exports.getDocumentFromReq(req, 'new');
+        this.dbservice.postObject(objectVal_, callbackFunc);
+        function callbackFunc(error, response) {
+          if (error) {
+          } else {
+          }
+        }
+      }
+    }
+    
     this.dbservice.postObject(objectVal, callbackFunc);
     function callbackFunc(error, response) {
       if (error) {
