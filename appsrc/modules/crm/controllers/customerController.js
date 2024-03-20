3f1ffc63b227df3484a926dcb7cce6b4f8557429
Haris Ahmad <haris@terminustech.com>
2024-03-20 14:50:07 +0500

    bug

--- appsrc/modules/crm/controllers/customerController.js
+++ appsrc/modules/crm/controllers/customerController.js
@@ -589,6 +589,13 @@ function getDocumentFromReq(req, reqType){
       var reqMainSite = {};
       reqMainSite.body = mainSite;
       reqMainSite.body.loginUser = req.body.loginUser;
+      
+      if(req?.body?.primaryTechnicalContact && ObjectId.isValid(req?.body?.primaryTechnicalContact))
+        reqMainSite.body.primaryTechnicalContact = req.body.primaryTechnicalContact;
+    
+      if(req?.body?.primaryBillingContact && ObjectId.isValid(req?.body?.primaryBillingContact))
+        reqMainSite.body.primaryBillingContact = req.body.primaryBillingContact;
+
       doc.mainSite = customerSiteController.getDocumentFromReq(reqMainSite, 'new');
       doc.mainSite.customer = doc._id;
     }
