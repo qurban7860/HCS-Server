959ef809e3b7924c7776f2e944fa0b7af6b4ecb9
Haris Ahmad <haris@terminustech.com>
2024-03-25 22:46:13 +0500

    console added.

--- appsrc/modules/documents/controllers/documentController.js
+++ appsrc/modules/documents/controllers/documentController.js
@@ -220,11 +220,15 @@ exports.getDocuments = async (req, res, next) => {
     // let documents = await dbservice.getObjectList(req, Document, this.fields, this.query, this.orderBy, this.populate);
     let docTypes_ = await DocumentType.find({ isPrimaryDrawing: true }).select('_id').lean();
 
+    console.log("docTypes_", docTypes_);
     let assemblyDrawings = await Document.find({ ...this.query, docType: { $in: docTypes_ } })
       .populate(this.populate)
       .sort({ "createdAt": -1 })
       .select(this.fields)
       .lean();
+
+      console.log("assemblyDrawings", assemblyDrawings);
+    
 
     let otherDocuments = await Document.find({ ...this.query, docType: { $nin: docTypes_ } })
       .populate(this.populate)
