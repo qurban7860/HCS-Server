0919a3a82a2d500bc0e22eebc9825f4f8c59030a
Haris Ahmad <haris@terminustech.com>
2024-05-02 19:28:11 +1200

    pm2 logs testing.

--- appsrc/modules/log/controllers/pm2LogController.js
+++ appsrc/modules/log/controllers/pm2LogController.js
@@ -99,14 +99,18 @@ function readFile(pageNumber, pageSize, stream, callback) {
     lines.reverse();
     const start = pageNumber * pageSize;
     const end = start + pageSize;
-    // const pageLines = lines.slice(start, end);
-    const pageLines = lines.slice(start, end).join('\n');
+    let pageLines = lines.slice(start, end);
+    pageLines.sort((a, b) => b.localeCompare(a));
+    const resultString = pageLines.join('\n');
+    // const pageLines = lines.slice(start, end).join('\n');
+
+
 
     if (start >= lines.length) {
       callback(null);
     } else {
       const returnResponce = {
-        data: pageLines,
+        data: resultString,
         totalPages: Math.ceil(lines.length / pageSize),
         currentPage: pageNumber,
         pageSize: pageSize,
