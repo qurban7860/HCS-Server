9a3acb3dedae862794d482df88f5f99ff141ceca
Ali Hassan <a.hassan.dev@outlook.com>
2024-06-06 11:29:01 +0500

    Jira Status test case revised

--- appsrc/modules/jira/controllers/jiraController.js
+++ appsrc/modules/jira/controllers/jiraController.js
@@ -94,15 +94,15 @@ exports.getTickets = async (req, res, next) => {
 
     let JQL = `project = ${jiraProject}`;
 
-    if(req?.query?.status?.trim() === 'Open' && req?.query?.status?.toLowerCase()?.trim() !== 'all'){
-      JQL +=" AND status in ('To Do','In Progress')";
-    }else if( req?.query?.status?.toLowerCase()?.trim() !== 'all') {
-      JQL +=` AND status = ${req?.query?.status?.trim()}`;  
-    }
-
-    // if (req?.query?.status?.trim().length > 0 && req?.query?.status?.toLowerCase()?.trim() !== 'all') {
-    //   JQL += ` AND ("status" = '${req.query.status}')`;
+    // if(req?.query?.status?.trim() === 'Open' && req?.query?.status?.toLowerCase()?.trim() !== 'all'){
+    //   JQL +=" AND status in ('To Do','In Progress')";
+    // }else if( req?.query?.status?.toLowerCase()?.trim() !== 'all') {
+    //   JQL +=` AND status = ${req?.query?.status?.trim()}`;  
     // }
+
+    if (req?.query?.status?.trim().length > 0 && req?.query?.status?.toLowerCase()?.trim() !== 'all') {
+      JQL += ` AND ("status" = '${req.query.status}')`;
+    }
 
     if(req?.query?.serialNo?.trim().length > 0){
       JQL += ` AND "Serial No[Short text]" ~ "${req?.query?.serialNo}"`;
