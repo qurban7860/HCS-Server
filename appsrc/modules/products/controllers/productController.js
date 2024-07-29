baf3274a2daa78aeb936d82d76450046c1535472
Ali Hassan <a.hassan.dev@outlook.com>
2024-07-29 17:03:58 +0500

    space removed

--- appsrc/modules/products/controllers/productController.js
+++ appsrc/modules/products/controllers/productController.js
@@ -1717,9 +1717,9 @@ exports.sendEmailAlert = async ( data ) => {
     const transferredDate = data?.transferredDate ? `<strong>Transferred Date: </strong> ${ data?.transferredDate } <br>` : '';
 
     if(data?.transferredDate){
-      text = `Machine ${serialNo} has been transferred from <strong>${data?.previousCustomer || '' }</strong> to <strong>${data?.customer || '' }</strong> .`
+      text = `Machine ${serialNo} has been transferred from <strong>${data?.previousCustomer || '' }</strong> to <strong>${data?.customer || '' }</strong>.`
     } else {
-      text = `Machine status has been changed from <strong>${ data?.previousStatus }</strong>  to <strong>${data?.status}</strong> .`
+      text = `Machine status has been changed from <strong>${ data?.previousStatus }</strong>  to <strong>${data?.status}</strong>.`
     }
 
     const contentHTML = await fs.promises.readFile(path.join(__dirname, '../../email/templates/machine.html'), 'utf8');
