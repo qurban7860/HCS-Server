8c0f5435afe019af77fed3dafd170d88cd84b8c4
Mubashir Akmal <mubashir@terminustech.com>
2024-12-06 16:03:28 +0500

    added logging for external import logs too

--- appsrc/modules/productLogs/controllers/publicLogController.js
+++ appsrc/modules/productLogs/controllers/publicLogController.js
@@ -7,6 +7,8 @@ const logger = require("../../config/logger");
 let logDBService = require("../service/logDBService");
 this.dbservice = new logDBService();
 const { CoilLog, ErpLog, ProductionLog, ToolCountLog, WasteLog } = require("../models");
+const APILog = require("../../apiclient/models/apilog");
+
 
 this.debug = process.env.LOG_TO_CONSOLE != null && process.env.LOG_TO_CONSOLE != undefined ? process.env.LOG_TO_CONSOLE : false;
 
@@ -21,6 +23,9 @@ this.populate = [
 ];
 
 exports.postPublicLog = async (req, res, next) => {
+  const startTime = Date.now();
+  const clientIP = req.clientInfo?.ip;
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(StatusCodes.BAD_REQUEST).send(getReasonPhrase(StatusCodes.BAD_REQUEST));
@@ -43,7 +48,7 @@ exports.postPublicLog = async (req, res, next) => {
 
     logs.forEach((logObj) => {
       logObj = convertTimestampToDate(logObj);
-      
+
       logObj.machine = req.machine._id;
       logObj.customer = req.customer._id;
       logObj.type = type;
@@ -60,14 +65,59 @@ exports.postPublicLog = async (req, res, next) => {
       await Model.create(logsToInsert);
     }
 
+
+    await logApiCall({
+      req,
+      startTime,
+      responseData: {
+        statusCode: StatusCodes.CREATED,
+        body: {
+          message: "Machine logs processed successfully",
+          count: logsToInsert.length,
+        },
+        context: `Successfully processed ${logsToInsert.length} ${type} logs`,
+      },
+      machine: req.machine,
+      createdIP: clientIP,
+    });
+
     res.status(StatusCodes.CREATED).json({
       message: "Machine logs processed successfully",
       count: logsToInsert.length,
     });
   } catch (error) {
+    await logApiCall({
+      req,
+      startTime,
+      responseData: {
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        body: error.message,
+        context: "Error Processing Machine Logs",
+      },
+      machine: req.machine,
+      createdIP: clientIP,
+    });
     logger.error(new Error(error));
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error.message);
   }
+};
+
+const logApiCall = async ({ req, startTime, responseData, machine = null, createdIP, createdBy }) => {
+  const apiLog = new APILog({
+    requestMethod: req.method,
+    requestURL: req.originalUrl,
+    requestHeaders: req.headers,
+    machine: machine ? [machine._id] : [],
+    customer: machine?.customer,
+    apiType: "MACHINE-LOGS",
+    responseTime: `${Date.now() - startTime}`,
+    response: JSON.stringify(responseData.body),
+    responseStatusCode: responseData.statusCode,
+    additionalContextualInformation: responseData.context,
+    createdIP: req?.clientInfo?.ip || createdIP,
+    createdBy: req?.clientInfo?.identifier || createdBy || null,
+  });
+  return apiLog.save();
 };
 
 function addIdentifierData(req) {
