bedafb78df05501c41e1801c60a5cacc3954a52f
Ali Hassan <a.hassan.dev@outlook.com>
2025-02-13 13:24:03 +0500

    changes revised!

--- appsrc/middleware/validateCustomerInQuery.js
+++ appsrc/middleware/validateCustomerInQuery.js
@@ -1,14 +1,34 @@
 const { StatusCodes } = require('http-status-codes');
 let rtnMsg = require('../modules/config/static/static')
 const logger = require('../modules/config/logger');
+const { SecurityUser } = require('../modules/security/models');
 
 module.exports = async (req, res, next) => {
   try {
 
-    if (!req.query.customer) {
+    if (req?.query?.customer) {
+      const userId = req.body.loginUser?.userId
+      if (!userId)
+        return res.status(StatusCodes.UNAUTHORIZED).send("Not Authorized!");
+
+      const record = await SecurityUser.findById(userId).populate({ path: 'customer', select: 'name type isActive isArchived' })
+
+      if (!record)
+        return res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Customer not found!', true));
+
+      if (!Array.isArray(record.customer) && record.customer?._id?.toString() !== req?.query?.customer?.toString())
+        return res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Invalid Customer!', true));
+
+      if (Array.isArray(record.customer) && record.customer?.every(CID => CID?._id?.toString() != req?.query?.customer?.toString()))
+        return res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Invalid Customer!', true));
+
+      if (_.isEmpty(record.customer) || record.customer.isActive == false || record.customer.isArchived == true)
+        return res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Customer verifcation failed!', true));
+
+      next();
+    } else {
       return res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Customer not found!', true));
     }
-    next();
 
   } catch (error) {
     logger.error(new Error(error));
--- appsrc/middleware/validateCustomerInRequest.js
+++ appsrc/middleware/validateCustomerInRequest.js
@@ -6,11 +6,29 @@ const logger = require('../modules/config/logger');
 module.exports = async (req, res, next) => {
   try {
 
-    if (!req?.query.customer) {
+    if (req?.body?.customer) {
+      const userId = req.body.loginUser?.userId
+      if (!userId)
+        return res.status(StatusCodes.UNAUTHORIZED).send("Not Authorized!");
+
+      const record = await SecurityUser.findById(userId).populate({ path: 'customer', select: 'name type isActive isArchived' })
+
+      if (!record)
+        return res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Customer not found!', true));
+
+      if (!Array.isArray(record.customer) && record.customer?._id?.toString() !== req?.body?.customer?.toString())
+        return res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Invalid Customer!', true));
+
+      if (Array.isArray(record.customer) && record.customer?.every(CID => CID?._id?.toString() != req?.body?.customer?.toString()))
+        return res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Invalid Customer!', true));
+
+      if (_.isEmpty(record.customer) || record.customer.isActive == false || record.customer.isArchived == true)
+        return res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Customer verifcation failed!', true));
+
+      next();
+    } else {
       return res.status(StatusCodes.BAD_REQUEST).send(rtnMsg.recordCustomMessageJSON(StatusCodes.BAD_REQUEST, 'Customer not found!', true));
     }
-
-    next();
   } catch (error) {
     logger.error(new Error(error));
     return next(error);
