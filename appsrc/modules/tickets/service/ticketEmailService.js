ff0d9e6c6114d47bed08e8321fbb462602457afe
Ali Hassan <a.hassan.dev@outlook.com>
2025-02-20 16:39:05 +0500

    changes revised!

--- appsrc/modules/tickets/service/ticketEmailService.js
+++ appsrc/modules/tickets/service/ticketEmailService.js
@@ -38,7 +38,7 @@ class TicketEmailService {
       const ticketData = await this.dbservice.getObjectById(Ticket, this.fields, req.params.id, this.populate);
       this.query = { ticket: req.params.id, isActive: true, isArchived: false };
       const commentsList = await this.dbservice.getObjectList(req, TicketComment, this.fields, this.query, this.orderBy, this.populate);
-      const comments = commentsList?.map(c => `${c?.comment || ""} <strong>by: </strong> ${c?.updatedBy?.name || ""} / ${fDateTime(c?.updatedAt)} <br/>`).join("<br/>");
+      const comments = commentsList?.map(c => `${c?.comment || ""} <br/><strong>By: </strong> ${c?.updatedBy?.name || ""} / ${fDateTime(c?.updatedAt)} <br/>`).join("<br/>");
 
       const requestType = ticketData?.requestType?.name || ""
       const status = ticketData?.status?.name || ""
