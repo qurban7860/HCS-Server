4383f6399f98b5d65e873bc15be2f4df6b219851
Ali Hassan <a.hassan.dev@outlook.com>
2025-04-17 17:19:10 +0500

    change revised

--- appsrc/modules/cp/security/routes/securityUserRoute.js
+++ appsrc/modules/cp/security/routes/securityUserRoute.js
@@ -17,6 +17,8 @@ router.get(`${baseRoute}/`, validateCustomerInQuery, controller.getSecurityUsers
 // - /api/1.0.0/cp/security/users/:id
 router.get(`${baseRoute}/:id`, checkIDs(validate.id), validateCustomerInQuery, controller.getSecurityUser);
 
+// - /api/1.0.0/cp/security/users/:id
+router.patch(`${baseRoute}/:id`, checkIDs(validate.id), controller.patchSecurityUser);
 // - /api/1.0.0/cp/security/users/updatePassword/:id
 router.patch(`${baseRoute}/updatePassword/:id`, checkIDs(validate.id), controller.patchSecurityUser);
 
