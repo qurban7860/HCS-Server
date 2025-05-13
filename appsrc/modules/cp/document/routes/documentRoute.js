54d73a061a7d35ebe1801c302b365ceab237b5db
Ali Hassan <a.hassan.dev@outlook.com>
2025-05-13 10:54:33 +0500

    change revised

--- appsrc/modules/cp/document/routes/documentRoute.js
+++ appsrc/modules/cp/document/routes/documentRoute.js
@@ -5,11 +5,15 @@ const controller = controllers.documentController;
 const router = express.Router();
 const checkIDs = require('../../../../middleware/validateParamIDs');
 const validateMachineInQuery = require('../../../../middleware/validateMachineInQuery');
+const validateCustomerInQuery = require('../../../../middleware/validateCustomerInQuery');
 const validateMachineDrawingCustomer = require('../../../../middleware/validateMachineDrawingCustomer');
 const validate = require('../../utils/validate');
 const baseRoute = `/document`;
 
 router.use(checkAuth);
+
+// - /api/1.0.0/cp/documents/document/
+router.get(`${baseRoute}/`, validateMachineInQuery, controller.getDocuments);
 
 // - /api/1.0.0/cp/documents/document/:id
 router.get(`${baseRoute}/:id`, checkIDs(validate.id), validateMachineInQuery, validateMachineDrawingCustomer, controller.getDocument);
